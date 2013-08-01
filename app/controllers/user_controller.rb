86ec738adfa3fdf14908f8ded1b2ab32bc80f69d
Robert J Martin <robertmartinbc@shaw.ca>
2013-07-31 18:19:31 -0700

    Modified User controller.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,11 @@
-class UsersController < ApplicationController
+# class UsersController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
-    @assignments = @user.@assignments(current_user)  
-  end
-
-def index
-    @users = User.top_rated.paginate(page: params[:page], per_page: 10)
-  end
-end
+#  def show
+#    @user = User.find(params[:id])
+#    @assignments = @user.@assignments(current_user)  
+#  end
+#
+#  def index
+#    @users = User.top_rated.paginate(page: params[:page], per_page: 10)
+#  end
+# end
