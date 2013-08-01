3b6b966bfeb989c07f042bff1cbf4e37f6e76adc
Robert J Martin <robertmartinbc@shaw.ca>
2013-08-01 08:41:20 -0700

    Tried fixing devise.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class User < ActiveRecord::Base 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-
+ 
   # has_many :posts, :class_name => 'Assignment'
   # has_many :projects, :class_name => 'Assignment'
   has_many :assignments
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130729195535) do
+ActiveRecord::Schema.define(:version => 20130729194145) do
 
   create_table "assignments", :force => true do |t|
     t.string   "title"
@@ -41,14 +41,6 @@ ActiveRecord::Schema.define(:version => 20130729195535) do
   end
 
   add_index "comments", ["assignment_id"], :name => "index_comments_on_assignment_id"
-
-  create_table "transactions", :force => true do |t|
-    t.string   "transaction_type"
-    t.integer  "assignment_id"
-    t.integer  "writer_id"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
